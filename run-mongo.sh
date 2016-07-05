b977c0c6c89a6457edfc8fe4523b6ae4994691b4
Sandeep Joshi <sandeep.joshi@dcengines.net>
2016-07-05 18:01:47 +0530

    Cleanup old directories before starting new instance

--- run-mongo.sh
+++ run-mongo.sh
@@ -1,3 +1,6 @@
 #gdb --args ./mongod --config mongo.conf --storageEngine rocksdb
+/bin/rm mongo.log
+/bin/rm -rf /mnt/data/db/db
+#./mongod --setParameter diagnosticDataCollectionEnabled=false --config mongo.conf 
 ./mongod --config mongo.conf 
 
