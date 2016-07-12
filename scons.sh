ac62d4b46d9f72739d6e81b1b9dca71e532ff703
Sandeep Joshi <sandeep.joshi@dcengines.net>
2016-07-12 21:54:50 +0530

    Changes to dce_notes Added some notes on failed_tests, changes done for debugging

--- scons.sh
+++ scons.sh
@@ -1,3 +1,6 @@
 # allocator = tcmalloc/jemalloc doesnt work with address sanitizer
-# scons --allocator=system --sanitize=address --dbg=on --opt=off -j20 CPPPATH=/usr/local/include LIBPATH=/usr/local/lib
- scons --dbg=on --opt=off -j20 CPPPATH=/usr/local/include LIBPATH=/usr/local/lib
+scons --dbg=on --opt=off -j20  \
+ CPPPATH=/usr/local/include \
+ LIBPATH=/usr/local/lib \
+ --allocator=system --sanitize=address  
+
