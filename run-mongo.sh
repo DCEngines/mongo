ed6c19f61ac9cfa7810bcb93c859684f00a2663a
Sandeep Joshi <sandeep.joshi@dcengines.net>
2016-07-19 11:01:05 +0530

    Added dce_notes files on How to build and run mongo + offloader updated mongo.conf file

--- run-mongo.sh
+++ run-mongo.sh
@@ -1,7 +1,6 @@
-#gdb --args ./mongod --config mongo.conf --storageEngine rocksdb
 /bin/rm mongo.log
 /bin/rm -rf /mnt/data/db/db
 #./mongod --setParameter diagnosticDataCollectionEnabled=false --config mongo.conf 
 gdb --args ./mongod --config mongo.conf 
-#nohup ./mongod --config mongo.conf &
+#./mongod --config mongo.conf 
 
